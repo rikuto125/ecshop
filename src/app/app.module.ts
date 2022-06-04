bf5f53d6e83c96ee63d94783d56373b46641de55
rikuto <wjrikuto@gmail.com>
2022-06-04 10:14:57 +0900

    Angularフロントエンド作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from 'src/app/common/navbar/navbar.component';
+import { ProductListingsComponent } from './product/product-listings/product-listings.component';
+import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {ProductListingsComponent} from "./product-listings/product-listings.component";
+import {RouterModule, Routes} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {ProductComponent} from "./product.component";
+
+const routes: Routes = [
+  { path: 'products', component: ProductComponent,
+  children:[
+    { path: '', component: ProductListingsComponent },
+    { path: ':productId', component: ProductDetailComponent },
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductDetailComponent,
+    ProductListingsComponent,
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
